8aebbb625cdb5e0a24f82806e01080f52120947b
Donghyun Lee <lee588779@naver.com>
2024-05-07 18:14:19 +0900

    Entity 구성

--- src/main/java/me/donghyunLee/springbootdeveloper/domain/Article.java
+++ src/main/java/me/donghyunLee/springbootdeveloper/domain/Article.java
@@ -1,7 +1,8 @@
 package me.donghyunLee.springbootdeveloper.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,4 +10,25 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
