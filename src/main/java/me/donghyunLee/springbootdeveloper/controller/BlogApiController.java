faa848ebb0a1f5bd6459da4f3576565b18e06877
Donghyun Lee <lee588779@naver.com>
2024-05-08 16:10:52 +0900

    서비스, 컨트롤러 메서드 작성

--- src/main/java/me/donghyunLee/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/donghyunLee/springbootdeveloper/controller/BlogApiController.java
@@ -1,9 +1,24 @@
 package me.donghyunLee.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.donghyunLee.springbootdeveloper.domain.Article;
+import me.donghyunLee.springbootdeveloper.dto.AddArticleRequest;
+import me.donghyunLee.springbootdeveloper.service.BlogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class BlogApiController {
+    private final BlogService blogService;
+
+    @PostMapping("/api/articles")
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
 }
--- src/main/java/me/donghyunLee/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/donghyunLee/springbootdeveloper/service/BlogService.java
@@ -1,5 +1,17 @@
 package me.donghyunLee.springbootdeveloper.service;
 
+import lombok.RequiredArgsConstructor;
+import me.donghyunLee.springbootdeveloper.domain.Article;
+import me.donghyunLee.springbootdeveloper.dto.AddArticleRequest;
+import me.donghyunLee.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class BlogService {
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
 }
