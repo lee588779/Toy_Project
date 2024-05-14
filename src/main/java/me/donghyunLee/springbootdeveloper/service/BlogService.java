92c301dd497c6a6df2b375232da7ed0a775af165
Donghyun Lee <lee588779@naver.com>
2024-05-14 19:19:55 +0900

    서비스 메서드 finlAll() 추가

--- src/main/java/me/donghyunLee/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/donghyunLee/springbootdeveloper/service/BlogService.java
@@ -6,12 +6,19 @@ import me.donghyunLee.springbootdeveloper.dto.AddArticleRequest;
 import me.donghyunLee.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
     private final BlogRepository blogRepository;
 
-    public Article save(AddArticleRequest request){
+    public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
+
     }
 }
