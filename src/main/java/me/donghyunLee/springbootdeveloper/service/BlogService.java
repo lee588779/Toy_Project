6d1fb3fddd1b64e05da84a4882d94b14d84cf4e7
Mr. <lee588779@naver.com>
2024-05-17 16:53:03 +0900

    서비스 메서드 코드 작성 _ update() _ 2

--- src/main/java/me/donghyunLee/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/donghyunLee/springbootdeveloper/service/BlogService.java
@@ -1,8 +1,10 @@
 package me.donghyunLee.springbootdeveloper.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import me.donghyunLee.springbootdeveloper.domain.Article;
 import me.donghyunLee.springbootdeveloper.dto.AddArticleRequest;
+import me.donghyunLee.springbootdeveloper.dto.UpdateArticleRequest;
 import me.donghyunLee.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
@@ -29,5 +31,15 @@ public class BlogService {
 
     public void delete(long id){
         blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request){
+        Article article = blogRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("not found : " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
