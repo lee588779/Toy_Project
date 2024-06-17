c36b8ee1f100bb9382ee4ac3719ad0537df65d8a
Mr. <lee588779@naver.com>
2024-06-17 16:42:27 +0900

    컨트롤러 메서드 작성 _ UserApiController.java _ signup()

---
+++ src/main/java/me/donghyunLee/springbootdeveloper/controller/UserApiController.java
@@ -0,0 +1,19 @@
+package me.donghyunLee.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.donghyunLee.springbootdeveloper.dto.AddUserRequest;
+import me.donghyunLee.springbootdeveloper.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class UserApiController {
+    private final UserService userService;
+
+    @PostMapping("/user")
+    public String signup(AddUserRequest request){
+        userService.save(request);
+        return "redirect:/login";
+    }
+}
