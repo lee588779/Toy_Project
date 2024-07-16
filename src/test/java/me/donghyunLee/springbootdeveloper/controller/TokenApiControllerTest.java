bb3b4900e51049e17c09aa75d3f13bea3a655438
Mr. <lee588779@naver.com>
2024-07-16 17:42:00 +0900

    테스트 코드 수정 _ TokenApiControllerTest.java _ import 오류 수정

--- src/test/java/me/donghyunLee/springbootdeveloper/controller/TokenApiControllerTest.java
+++ src/test/java/me/donghyunLee/springbootdeveloper/controller/TokenApiControllerTest.java
@@ -16,15 +16,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Map;
 
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -69,7 +68,6 @@ public class TokenApiControllerTest {
 
         CreateAccessTokenRequest request = new CreateAccessTokenRequest();
         request.setRefreshToken(refreshToken);
-
         final String requestBody = objectMapper.writeValueAsString(request);
 
         ResultActions resultActions = mockMvc.perform(post(url)
