17ce5e4d9c0c871fc3d8f5d88ab3e137e9151bad
Mr. <lee588779@naver.com>
2024-07-16 18:00:04 +0900

    쿠키 관리 클래스 구현 _ CookieUtil.java

---
+++ src/main/java/me/donghyunLee/springbootdeveloper/util/CookieUtil.java
@@ -0,0 +1,46 @@
+package me.donghyunLee.springbootdeveloper.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.SerializationUtils;
+
+import java.util.Base64;
+
+public class CookieUtil {
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge){ //요청값을 바탕으로 쿠키 추가
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name){ //이름을 입력받아 쿠키 삭제
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null){
+            return;
+        }
+
+        for (Cookie cookie : cookies){
+            if (name.equals(cookie.getName())){
+                cookie.setValue("");
+                cookie.setPath("/");
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+    }
+
+    public static String serialize(Object obj){ //객체 직렬화 -> 쿠키 값 변환
+        return Base64.getUrlEncoder()
+                .encodeToString(SerializationUtils.serialize(obj));
+    }
+
+    public static <T> T deserialize(Cookie cookie, Class<T> cls){ // 쿠키 역직렬화 -> 객체로 변환
+        return cls.cast(
+                SerializationUtils.deserialize(
+                        Base64.getUrlDecoder().decode(cookie.getValue())
+                )
+        );
+    }
+}
