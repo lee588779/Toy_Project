dc6c48386fe9796434931ce9f641afdf4080636c
Mr. <lee588779@naver.com>
2024-08-07 18:36:14 +0900

    쿠키 추가 함수, HTTP 요청 함수 추가 _ article.js

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -58,3 +58,60 @@ if (createButton) {
         });
     }
 }
+
+function getCookie(key) {
+    var result = null;
+    var cookie = document.cookie.split(';');
+    cookie.some(function (item) {
+        item = item.replace(' ', '');
+
+        var dic = item.split('=');
+
+        if (key === dic[0]) {
+            result = dic[1];
+            return true;
+        }
+    });
+
+    return result;
+}
+
+function httpRequest(method, url, body, success, fail) {
+    fetch(url, {
+        method: method,
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+            'Content-Type': 'application/json',
+        },
+        body: body,
+    }).then(response => {
+        if (response.status === 200 || response.status === 201) {
+            return success();
+        }
+        const refresh_token = getCookie('refresh_token');
+        if (response.status === 401 && refresh_token) {
+            fetch('/api/token', {
+                method: 'POST',
+                headers: {
+                    Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    refreshToken: getCookie('refresh_token'),
+                }),
+            })
+                .then(res => {
+                    if (res.ok) {
+                        return res.json();
+                    }
+                })
+                .then(result => {
+                    localStorage.setItem('access_token', result.accessToken);
+                    httpRequest(method, url, body, success, fail);
+                })
+                .catch(error => fail());
+        } else {
+            return fail();
+        }
+    });
+}
